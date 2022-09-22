83011e1168989765d2f16ad093f6b218f48d6fd4
SIVASANKAR-JAYAVEL <113333564+sivasankar-jayavel@users.noreply.github.com>
2022-09-23 00:37:55 +0530

    Add files via upload

---
+++ TASK 3.1.js
@@ -0,0 +1,62 @@
+{
+    "basics": {
+    "name": "sivasankar j",
+    "email": "sivasankar6104@gmail.com",
+    "phone": "9876543210",
+    "degree": "civil engineering",
+    "dob":"1997-06-28"
+    "summary": "to be a successful professional in a globally respected company and to help the company to achieve the objective.",
+    "location": {
+    "address": "1-amman nagar,pattanam",
+    "postalCode": "637408",
+    "city": "namakkal",
+    "region": "tamilnadu"
+    },
+    },
+    "work": [
+    {
+    "company": "c2r construction",
+    "position": "civil engineer",
+    "startDate": "2018-08-25",
+    "endDate": "still working",
+    "summary": "co- ordinating with client,supervising day to day on-site work,Planning and execution of  work as per design and drawing through plan.",
+    }
+    ],
+    "education": [
+    {
+    "institution": "gnanamani college of technology",
+    "area": "namakkal",
+    "studyType": "regular",
+    "startDate": "2014-05-28",
+    "endDate": "2018-09-14",
+    "gpa": "6.68",
+    "courses":"civil engineering"
+    },
+    ],
+    "skills": [
+    {
+    "name": "autocad,revit,stadd pro,lumion,valuation",
+    }
+    ],
+    "languages": [
+    {
+    "language": "tamil,english",
+    "fluency": "read,write,speak"
+    }
+    ],
+    "interests": [
+    {
+    "name": "gym",
+    "keywords": [
+    "national level deadlift winner , national level body builder"
+    ]
+    }
+    ],
+    "projects":[
+        {
+        "name":"planning , designing, and analysis of three star hostel building",
+        "name":"project experimental investigation of light weight foamed cement mortar"
+        }
+    ],
+    },
+]
